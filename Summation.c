2565de270ebf7eebd2cfcd11074d1b55672c1a85
anutchai <anuschai.bsod@gmail.com>
2021-01-12 15:08:59 +0700

    first commit

---
+++ Summation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+
+    int x,y,sum;
+    
+    printf("Value of x is : ");
+    scanf("%d",&x);
+    
+    printf("Value of y is : ");
+    scanf("%d",&y);
+
+    sum = x + y;
+
+    printf("Output is %d\n", sum);
+
+}
+
+
