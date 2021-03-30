f348927fac3bb658570b8f3e2159c2969c09033b
anutchai <anuschai.bsod@gmail.com>
2021-03-30 12:52:58 +0700

    update quest

--- preprocessor/question.c
+++ preprocessor/question.c
@@ -11,46 +11,33 @@
 
 int grade(int scorce);
 
-int score[SIZE];
+int salary[SIZE];
 int student_no = 1;
 int main(){
 
 
-    printf("Input Student %d Score: ", student_no);
-        scanf("%d", &score[0]);
-    printf("Input Student %d Score: ", student_no+=1);
-        scanf("%d", &score[1]);
-    printf("Input Student %d Score: ", ++student_no);
-        scanf("%d", &score[2]);
-    grade(score[0]);
-    grade(score[1]);
-    grade(score[2]);
+    printf("Input salary %d : ", student_no);
+        scanf("%d", &salary[0]);
+    printf("Input salary %d : ", student_no+=1);
+        scanf("%d", &salary[1]);
+    printf("Input salary %d : ", ROUND);
+        scanf("%d", &salary[2]);
+    grade(salary[0]);
+    grade(salary[1]);
+    grade(salary[2]);
 
     return 0 ;
 }
 
-int grade(int fx_score){
+int grade(int fx_s){
 
-    if(fx_score>100){
-        printf("Error \n");
-        return 0;
-    }
-
-
-    if(fx_score>=80 && fx_score<=100){
-      printf("score grade 4 \n",fx_score);  
-    }
-    else if(fx_score>=70){
-         printf("score grade 3 \n",fx_score);
+  if(fx_s >= 500000){
+            printf("Don't get privilage\n");
+        }else if(fx_s >= 250000){
+            printf("Get privilage 5000 bath\n");
         }
-        else if(fx_score>=60) {
-            printf("score grade 2 \n",fx_score);
-        }
-        else if(fx_score>=50){
-             printf("score grade 1 \n",fx_score);
-             }
         else{
-             printf("score grade 0 \n",fx_score);
-             }
+            printf("Get privilage 7000 bath\n");
+        }
     return 0;
 }
